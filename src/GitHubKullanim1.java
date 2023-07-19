94aafbf2e4f7bf7151fcfe762a6b6044bcea508f
yusufrahman <yusufrahman0234@gmail.com>
2023-07-19 17:51:00 +0200

    main branch degisikilk

--- src/GitHubKullanim1.java
+++ src/GitHubKullanim1.java
@@ -1,6 +1,11 @@
 public class GitHubKullanim1 {
     public static void main(String[] args) {
 
+        System.out.println("Hello world");
+
+        System.out.println("That's it");
+        System.out.println("main branch degisiklik 1.satir");
+        System.out.println("main branch degisiklik 2.satir");
 
 
     }

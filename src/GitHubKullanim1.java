807c3dabdd6b9eb9dc8e7d1bc8a0a0c2871b4a7c
yusufrahman <yusufrahman0234@gmail.com>
2023-07-19 17:45:08 +0200

    yusuf branch 2.degisiklik

--- src/GitHubKullanim1.java
+++ src/GitHubKullanim1.java
@@ -1,5 +1,13 @@
+
 public class GitHubKullanim1 {
     public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+
+        System.out.println("That's it");
+
+        System.out.println("Yusuf branch'inde ortak class'da degisiklik yaptim");
 
 
 
--- src/ozellik1.java
+++ src/ozellik1.java
@@ -4,6 +4,7 @@
 public class ozellik1 {
     public static void main(String[] args) {
         System.out.println("yusuf branch'inda calisiyorum");
+        System.out.println("yusuf branchinda 2.degisiklik kendime ozel class'da");
     }
 
 }
